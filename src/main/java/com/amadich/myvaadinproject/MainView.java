5ad8eb3f8556ebdeba572a5ccf2d14b0ce7aeca1
Mohamed <mohamed@jeridi.tn>
2026-03-12 17:46:11 +0100

    v:0.0.1

--- src/main/java/com/amadich/myvaadinproject/MainView.java
+++ src/main/java/com/amadich/myvaadinproject/MainView.java
@@ -1,23 +1,31 @@
 package com.amadich.myvaadinproject;
 
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.avatar.Avatar;
 import com.amadich.myvaadinproject.backend.model.DeployRequest;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.*;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Random;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.*;
 @Route("")
 @PageTitle("DevOps Self Service Portal")
 public class MainView extends VerticalLayout {
@@ -27,10 +35,19 @@ public class MainView extends VerticalLayout {
         setSizeFull();
         setAlignItems(Alignment.CENTER);
 
-        H1 title = new H1("DevOps Self Service Portal");
+
+        H1 title = new H1("INBOX Hosting - Self Service Portal");
+        title.getStyle().set("color", "darkblue");
+ProgressBar progressbar = new ProgressBar();
+progressbar.setValue(0.75);
+
+        Avatar avatar = new Avatar("","https://dash.inbox.com.iq/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Finbox_main_fix_network.8cf7b05c.png&w=1200&q=75");
+        avatar.setHeight("96px");
+        avatar.setWidth("96px");
 
         TextField projectName = new TextField("Project Name");
         projectName.setWidth("300px");
+
 
         Grid<ServiceType> grid = new Grid<>(ServiceType.class, false);
 
@@ -63,14 +80,19 @@ public class MainView extends VerticalLayout {
 
         grid.setItems(ServiceType.values());
         grid.setWidth("900px");
+        grid.getStyle().set("margin", "0 auto");
+add(title, progressbar, avatar, projectName, grid);
 
-        add(title, projectName, grid);
     }
 
     private void showServiceForm(ServiceType service, String project) {
 
         Dialog dialog = new Dialog();
+
+
         dialog.setWidth("650px");
+        dialog.getElement().getStyle().set("margin-left", "0");
+        dialog.getElement().getStyle().set("margin-right", "auto");
 
         VerticalLayout layout = new VerticalLayout();
 
@@ -97,7 +119,7 @@ public class MainView extends VerticalLayout {
 
         form.add(projectField);
 
-        switch (service) {
+        switch (service){
 
             case POSTGRES -> {
 
